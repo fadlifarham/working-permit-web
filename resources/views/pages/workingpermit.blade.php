b1727ddb55fa2998939140cc631de735f18e49d2
fadli7 <fadlifarham10@gmail.com>
2019-01-24 15:04:03 +0700

    fix wp pages

--- resources/views/pages/workingpermit.blade.php
+++ resources/views/pages/workingpermit.blade.php
@@ -7,30 +7,65 @@
                 Working Permit
             </h1>
         </section>
-    </div>
-    <section class="content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="box box-danger">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Different Width</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <input type="text" class="form-control" placeholder=".col-xs-3">
-                            </div>
-                            <div class="col-xs-4">
-                                <input type="text" class="form-control" placeholder=".col-xs-4">
-                            </div>
-                            <div class="col-xs-5">
-                                <input type="text" class="form-control" placeholder=".col-xs-5">
-                            </div>
+
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="box box-warning">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Isi Data Anda</h3>
                         </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <form action="" method="" role="form">
+
+                                <div class="form-group">
+                                    <label>Uraian Pekerjaan</label>
+                                    <textarea class="form-control" rows="3" placeholder="Masukkan Uraian Pekerjaan Anda"></textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Lokasi</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan Lokasi Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Area Kerja</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan Area Kerja Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Manager</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan Manager Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Pengawas</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan Pengawas Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>K3</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan K3 Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>App</label>
+                                    <input type="text" class="form-control" placeholder="Masukkan App Anda">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Lokasi</label>
+                                    <input type="text" class="form-control" placeholder="Enter ...">
+                                </div>
+
+                            </form>
+                        </div>
+                        <!-- /.box-body -->
                     </div>
-                    <!-- /.box-body -->
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
+
 @endsection
