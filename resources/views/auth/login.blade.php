8836ba562e4fb4740612b9a0c7f885006250a767
dinopuguh <puguhmunung16@gmail.com>
2019-01-27 22:25:49 +0700

    add auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,17 @@
 @extends('LogRegLayout')
 
 @section('body')
-    <body class="hold-transition register-page">
-    <div class="register-box">
-        <div class="register-logo">
+    <body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
             <b>Working</b> Permit</a>
         </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
 
-        <div class="register-box-body">
-            <p class="login-box-msg">Register a new membership</p>
-
-            <form method="POST" action="{{ route('register') }}">
+            <form method="POST" action="{{ route('login') }}">
                 @csrf
-                <div class="form-group has-feedback">
-                    <input type="text" name="name" class="form-control" placeholder="Name">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
                 <div class="form-group has-feedback">
                     <input type="email" name="email" class="form-control" placeholder="Email">
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
@@ -24,31 +20,30 @@
                     <input type="password" name="password" class="form-control" placeholder="Password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 </div>
-                <div class="form-group has-feedback">
-                    <input type="password" name="password_confirmation" class="form-control" placeholder="Retype password">
-                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-                </div>
                 <div class="row">
                     <div class="col-xs-8">
                         <div class="checkbox icheck">
                             <label>
-                                <input type="checkbox"> I agree to the <a href="#">terms</a>
+                                <input type="checkbox" name="remember"> Remember Me
                             </label>
                         </div>
                     </div>
                     <!-- /.col -->
                     <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
+            <!-- /.social-auth-links -->
 
-            <a href="{{ url('/login') }}" class="text-center">I already have a membership</a>
+            {{--<a href="">I forgot my password</a><br>--}}
+            <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
+
         </div>
-        <!-- /.form-box -->
+        <!-- /.login-box-body -->
     </div>
-    <!-- /.register-box -->
+    <!-- /.login-box -->
 
     <!-- jQuery 3 -->
     <script src="{{ url('bower_components/jquery/dist/jquery.min.js') }}"></script>
